5cc6795d46ea501b86f5626228e38ae6e4c294f5
Mohammed Adil <sayedadil0407@gmail.com>
2026-05-14 11:47:32 +0530

    Getting a hang of it

--- Loops.java
+++ Loops.java
@@ -54,7 +54,11 @@ public class Loops {
 
         for (int i =1;i<=4;i++){
             for (int j =1; j<=5;j++){
-                System.out.print("*");
+                if (i ==1 || j==1 || i == 4 || j ==5){
+                    System.out.print("*"); }
+                    else {
+                        System.out.print(" ");
+                    }
             }
             System.out.println();
         }
